29ac05481d96e249f242cc795628eff2abff822f
dilsuz <dilsuz25012303@gmail.com>
2023-03-25 10:34:43 +0500

    Life circle method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+            show : true
+
+            // favouriteColor : 'oq'
+            // posts: [],
+            // loading: true
+        }
+    }
+
+    getData = () => {
+        this.setState({show: false})
+    }
+
+    // getData() {
+    //     setTimeout(() => {
+    //         console.log('Bu ma`lumot yangilanyapti');
+    //         this.setState({ data: 'Dilso`z' })
+    //     }, 3000)
+    // }
+
+    componentDidMount() {
+        // this.getData()
+        console.log('componentdDidMount');
+
+        // setTimeout(() => {
+        //     this.setState({favouriteColor : 'ko`k'})
+        // },3000)
+
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        //     .then(response => response.json())
+        //     .then(data => this.setState({posts : data,loading :false}))
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+        // document.querySelector('h1').innerHTML = `rang ${this.state.favouriteColor}`
+        
+    }
+
+    render() {
+        let myHeader;
+        if(this.state.show) {
+            myHeader = <Child />
+        }
+        return (
+            <div>
+                {myHeader}
+                <button onClick={this.getData}>Delete Header</button>
+            </div>
+        )
+    }
+
+    // render() {
+    //     return (
+    //         <div>
+    //             <h1>Yoqtirgan rang {this.state.favouriteColor}</h1>
+    //             {/* {
+    //                 this.state.loading ? 
+    //                 <h3>Loading ...</h3> :
+    //                 <h3>{this.state.posts.length}</h3>
+    //             } */}
+    //         </div>
+    //     )
+    // }
 }
 
-export default App;
+class Child extends Component {
+    componentWillUnmount(){
+        alert('Bu componenta o`chadi')
+    }
+    render() {
+        return <h1>Salom</h1>
+    }
+}
+
+export default App
